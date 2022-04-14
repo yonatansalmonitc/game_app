d17144e9e51d13fb56ff6881ddd2beb8498d0de1
Yonatan Salmon <yonatansalmon@itc.tech>
2022-04-14 10:49:24 +0300

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 
 
+const cors = require('cors');
+
+
 app.use('something', route)
 app.listen(8000, () => {
     console.log('Listening')
