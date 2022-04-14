b5c95fcd6c3092fdbb5bf1d072854114af4065d0
Yonatan Salmon <yonatansalmon@itc.tech>
2022-04-14 10:46:13 +0300

    added differernt route

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.use('/notes', notesRoute)
-
+app.use('something', route)
 app.listen(8000, () => {
     console.log('Listening')
 })
